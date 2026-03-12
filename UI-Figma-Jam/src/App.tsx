91002d100659c5d7a9b100de5ecd4fdcbf4176dd
João Ramalho <joaoaugustovr@gmail.com>
2026-03-11 23:54:42 -0300

    Add Square node and flow state & connections
    Introduce a reusable Square node component with four connection handles and integrate it into the ReactFlow canvas. App.tsx now registers nodeTypes, defines initialNodes (using crypto.randomUUID), and manages nodes/edges via useNodesState/useEdgesState. Added onConnect handler (addEdge), passed nodes/edges and change handlers to ReactFlow, set connectionMode to Loose, and adjusted Background styling.

--- UI-Figma-Jam/src/App.tsx
+++ UI-Figma-Jam/src/App.tsx
@@ -1,12 +1,48 @@
-import { ReactFlow, Background, Controls } from '@xyflow/react';
+import { ReactFlow,Background, Controls, type Node, type NodeTypes, useEdgesState, type Connection, useNodesState,addEdge,ConnectionMode,type Edge} from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
+import { Square } from './components/nodes/Square';
+import { useCallback } from 'react';
+
+const nodeTypes : NodeTypes = {
+  Square : Square,
+};
+
+const initialNodes : Node[] = [
+  { 
+    id : crypto.randomUUID(),
+    type : 'Square',
+    position : { x : 200, y : 400 },
+    data : {label : ''},
+  },
+  {
+    id : crypto.randomUUID(),
+    type : 'Square',
+    position : { x : 900, y : 400 },
+    data : {label : ''},
+  },
+] satisfies Node[]
 
 function App() {
+ const [edges, setEdges, onEdgeChange] = useEdgesState([])
+ const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
+
+ const Onconnect = useCallback((connection : Connection) => {
+   return setEdges(edges => addEdge(connection, edges))
+ }, [])
+
   return (
-<div className='w-screen h-screen '>
-      <ReactFlow>
-        <Background  gap={12} size={2} color='#000'/>
+    <div className='w-screen h-screen '>
+      <ReactFlow
+       nodeTypes={nodeTypes}
+       nodes={nodes}
+       edges={edges}
+       onEdgesChange={onEdgeChange}
+       onConnect={Onconnect}
+       onNodesChange={onNodesChange}
+       connectionMode={ConnectionMode.Loose}
+       >
+        <Background  gap={12} size={1} color='#454545'/>
         <Controls />
       </ReactFlow>
     </div>
